d33750329f96865dd78b5204ca3bddc72acdbf17
vinayak-chavan <72243768+vinayak-chavan@users.noreply.github.com>
2023-05-14 12:40:43 +0530

    swagger documentation added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./db/connection');
 const express = require('express');
 const routes = require('./routes/event');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = 5000;
 
@@ -8,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use('/api/v3/app', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`connection is live at port ${port}`);
--- routes/event.js
+++ routes/event.js
@@ -3,11 +3,11 @@ const router = new express.Router();
 const { getEventById, getAllEvents, createEvent, updateEvent, deleteEvent } = require('../contollers/eventController');
 const uploadFunction = require('../middleware/imageUpload');
 
-router.get("/events/:id", getEventById);
+router.get("/event/:id", getEventById);
 router.get("/events", getAllEvents)
-router.post("/events", uploadFunction, createEvent);
+router.post("/event", uploadFunction, createEvent);
 router.put("/events/:id", uploadFunction, updateEvent);
-router.delete("/events/:id", deleteEvent);
+router.delete("/even/:id", deleteEvent);
 
 
 module.exports = router;
